242760a13138a169c43b8574f0a9612901ec1b02
iwbnwif <iwbnwif@gmail.com>
2016-08-28 17:43:28 +0100

    Convert tabs to spaces in all library header files.

--- include/wx/report/defs.h
+++ include/wx/report/defs.h
@@ -2,7 +2,7 @@
 #define DEFS_H
 
 #ifdef USING_SOURCE_RP
-	#define WXDLLIMPEXP_RP
+    #define WXDLLIMPEXP_RP
     #define WXDLLIMPEXP_DATA_RP(type) type
 #elif defined( LIB_USINGDLL )
     #define WXDLLIMPEXP_RP
@@ -22,93 +22,93 @@
 
 enum ItemsTypes
 {
-	wxRP_TEXT,
-	wxRP_PARAGRAPH,
-	wxRP_TEXTVALUE,
-	wxRP_IMAGE,
-	wxRP_TABLE,
-	wxRP_TABLECELL,
-	wxRP_LINESHAPE,
-	wxRP_RECTANGLESHAPE,
-	wxRP_CIRCLESHAPE
+    wxRP_TEXT,
+    wxRP_PARAGRAPH,
+    wxRP_TEXTVALUE,
+    wxRP_IMAGE,
+    wxRP_TABLE,
+    wxRP_TABLECELL,
+    wxRP_LINESHAPE,
+    wxRP_RECTANGLESHAPE,
+    wxRP_CIRCLESHAPE
 };
 
 enum SourcesTypes
 {
-	wxRP_WXARRAYSHORT,
-	wxRP_WXARRAYINT,
-	wxRP_WXARRAYLONG,
-	wxRP_WXARRAYDOUBLE,
-	wxRP_WXARRAYSTRING,
-	wxRP_ARRAYSHORT,
-	wxRP_ARRAYINT,
-	wxRP_ARRAYLONG,
-	wxRP_ARRAYFLOAT,
-	wxRP_ARRAYDOUBLE,
+    wxRP_WXARRAYSHORT,
+    wxRP_WXARRAYINT,
+    wxRP_WXARRAYLONG,
+    wxRP_WXARRAYDOUBLE,
+    wxRP_WXARRAYSTRING,
+    wxRP_ARRAYSHORT,
+    wxRP_ARRAYINT,
+    wxRP_ARRAYLONG,
+    wxRP_ARRAYFLOAT,
+    wxRP_ARRAYDOUBLE,
 };
 
 // Styles constants
 enum SidesValues
 {
-	wxRP_NOBORDER 	= 0,//0b0000,
-	wxRP_LEFTBORDER 	= 1,//0b0001,
-	wxRP_RIGHTBORDER 	= 2,//0b0010,
-	wxRP_TOPBORDER		= 4,//0b0100,
-	wxRP_BOTTOMBORDER	= 8,//0b1000,
-	wxRP_ALLBORDER		= 15,//0b1111,
-	wxRP_ALLINBORDER	= 16//0b10000 // for tables
+    wxRP_NOBORDER 	= 0,//0b0000,
+    wxRP_LEFTBORDER 	= 1,//0b0001,
+    wxRP_RIGHTBORDER 	= 2,//0b0010,
+    wxRP_TOPBORDER		= 4,//0b0100,
+    wxRP_BOTTOMBORDER	= 8,//0b1000,
+    wxRP_ALLBORDER		= 15,//0b1111,
+    wxRP_ALLINBORDER	= 16//0b10000 // for tables
 };
 
 enum TextAlign
 {
-	wxRP_LEFTALIGN = 0,
-	wxRP_CENTERALIGN = 1,
-	wxRP_RIGHTALIGN = 2,
-	wxRP_JUSTIFY = 3
+    wxRP_LEFTALIGN = 0,
+    wxRP_CENTERALIGN = 1,
+    wxRP_RIGHTALIGN = 2,
+    wxRP_JUSTIFY = 3
 };
 
 enum WrapMode
 {
-	wxRP_NOWRAP = 0,
-	wxRP_WRAPWHITESPACES = 1,
-	wxRP_WRAPANYWHERE = 2
+    wxRP_NOWRAP = 0,
+    wxRP_WRAPWHITESPACES = 1,
+    wxRP_WRAPANYWHERE = 2
 };
 
 enum wxReportPageParts
 {
-	wxRP_BODY,
-	wxRP_HEADER,
-	wxRP_FOOTER
+    wxRP_BODY,
+    wxRP_HEADER,
+    wxRP_FOOTER
 };
 
 enum VariableTypes
 {
-	wxRP_SHORT,
-	wxRP_INT,
-	wxRP_LONG,
-	wxRP_FLOAT,
-	wxRP_DOUBLE,
-	wxRP_WXSTRING
+    wxRP_SHORT,
+    wxRP_INT,
+    wxRP_LONG,
+    wxRP_FLOAT,
+    wxRP_DOUBLE,
+    wxRP_WXSTRING
 };
 
 enum Units
 {
-	wxRP_MM,
-	wxRP_INCH
+    wxRP_MM,
+    wxRP_INCH
 };
 
 enum PositionsArray
 {
-	wxRP_AT_LAST = -1
+    wxRP_AT_LAST = -1
 };
 
 enum wxReportPositions
 {
-	wxRP_LEFT = 10000,
-	wxRP_CENTER = 10001,
-	wxRP_RIGHT = 10002,
-	wxRP_TOP = 10003,
-	wxRP_BOTTOM = 10004
+    wxRP_LEFT = 10000,
+    wxRP_CENTER = 10001,
+    wxRP_RIGHT = 10002,
+    wxRP_TOP = 10003,
+    wxRP_BOTTOM = 10004
 };
 
 /**
@@ -117,21 +117,21 @@ enum wxReportPositions
 /*class wxReportUnit
 {
 private:
-	static double m_unitTypeRatio;
-	
+    static double m_unitTypeRatio;
+    
 public:
-	static void SetUnitsRatio(int unitType)
-	{
-		if(unitType == wxRP_INCH)
-			m_unitTypeRatio = 1.0;
-			
-		if(unitType == wxRP_MM)
-			m_unitTypeRatio = 25.4;
-	}
-	inline static double GetUnitsRatio()
-	{
-		return m_unitTypeRatio;
-	}
+    static void SetUnitsRatio(int unitType)
+    {
+        if(unitType == wxRP_INCH)
+            m_unitTypeRatio = 1.0;
+            
+        if(unitType == wxRP_MM)
+            m_unitTypeRatio = 25.4;
+    }
+    inline static double GetUnitsRatio()
+    {
+        return m_unitTypeRatio;
+    }
 };*/
 
 #endif // DEFS_H
--- include/wx/report/pageitems.h
+++ include/wx/report/pageitems.h
@@ -14,11 +14,11 @@
  */
 inline int MM2PX(double mm, wxDC *dc, bool toScreen)
 {
-	double sx = 1.0, sy = 1.0;
-	// if( !toScreen ) dc->GetUserScale(&sx, &sy);
+    double sx = 1.0, sy = 1.0;
+    // if( !toScreen ) dc->GetUserScale(&sx, &sy);
     dc->GetUserScale(&sx, &sy);
     
-	return wxRound((((double)(dc->GetPPI().x) / 25.4) * mm) / sx);
+    return wxRound((((double)(dc->GetPPI().x) / 25.4) * mm) / sx);
 }
 
 /*!
@@ -33,56 +33,56 @@ inline int MM2PX(double mm, wxDC *dc, bool toScreen)
  */
 inline void DrawBorder(wxDC *dc, int border, int x, int y, int w, int h)
 {
-	int x1, y1, x2, y2;
-	x1 = y1 = x2 = y2 = 0;
-	
-	if(border <= 0b1111 && border > 0)
-	{
-		if(border == 0b1111)
-		{			
-			dc->DrawRectangle(x, y, w, h);
-		}
-		else
-		{
-			for(int i=0b0001; i<0b1111; ++i)
-			{
-				int borderType = border & i;
-				
-				switch(borderType)
-				{
-					case wxRP_LEFTBORDER:
-						x1 = x;
-						y1 = y;
-						x2 = x;
-						y2 = y + h;
-						break;
-						
-					case wxRP_RIGHTBORDER:
-						x1 = x + w;
-						y1 = y;
-						x2 = x1;
-						y2 = y + h;
-						break;
-						
-					case wxRP_TOPBORDER:
-						x1 = x;
-						y1 = y;
-						x2 = x + w;
-						y2 = y;
-						break;
-						
-					case wxRP_BOTTOMBORDER:
-						x1 = x;
-						y1 = y + h;
-						x2 = x + w;
-						y2 = y1;
-				}
-				
-				if(borderType > 0)
-					dc->DrawLine(x1, y1, x2, y2);
-			}
-		}
-	}	
+    int x1, y1, x2, y2;
+    x1 = y1 = x2 = y2 = 0;
+    
+    if(border <= 0b1111 && border > 0)
+    {
+        if(border == 0b1111)
+        {			
+            dc->DrawRectangle(x, y, w, h);
+        }
+        else
+        {
+            for(int i=0b0001; i<0b1111; ++i)
+            {
+                int borderType = border & i;
+                
+                switch(borderType)
+                {
+                    case wxRP_LEFTBORDER:
+                        x1 = x;
+                        y1 = y;
+                        x2 = x;
+                        y2 = y + h;
+                        break;
+                        
+                    case wxRP_RIGHTBORDER:
+                        x1 = x + w;
+                        y1 = y;
+                        x2 = x1;
+                        y2 = y + h;
+                        break;
+                        
+                    case wxRP_TOPBORDER:
+                        x1 = x;
+                        y1 = y;
+                        x2 = x + w;
+                        y2 = y;
+                        break;
+                        
+                    case wxRP_BOTTOMBORDER:
+                        x1 = x;
+                        y1 = y + h;
+                        x2 = x + w;
+                        y2 = y1;
+                }
+                
+                if(borderType > 0)
+                    dc->DrawLine(x1, y1, x2, y2);
+            }
+        }
+    }	
 }
 
 wxPoint CalcNegPos(const wxRealPoint& origPos, const wxPoint& pxPos, int w, int h, int lm, int tm, int rm, int bm, wxDC *dc);
@@ -97,79 +97,79 @@ wxPoint CalcNegPos(const wxRealPoint& origPos, const wxPoint& pxPos, int w, int 
 class WXDLLIMPEXP_RP wxReportPageItem
 {
 protected:
-	wxString m_sName;
-	int m_iType;
-	int m_iValueType;
-	wxString m_sValue;
-	bool m_fVariable;
-	const void* m_pVariable;
-	
+    wxString m_sName;
+    int m_iType;
+    int m_iValueType;
+    wxString m_sValue;
+    bool m_fVariable;
+    const void* m_pVariable;
+    
 public:
-	/**
-	 * \brief Default constructor.
-	 */
-	wxReportPageItem();
-	/**
-	 * \brief Constructor.
-	 * \param name name of the item
-	 * \param isVariable tells whether the item will be connected with data source object
-	 */
-	wxReportPageItem(const wxString& name, bool isVariable = false);
-	/**
-	 * \brief Virtual destructor.
-	 */
-	virtual ~wxReportPageItem() {;}
-	/**
-	 * \brief Set name of the item.
-	 * \param name name of the item
-	 */
-	void SetName(const wxString& name);
-	/**
-	 * \brief Sets if the item will be filled from data.
-	 * \param isVariable TRUE to enable, FALSE to disable
-	 */
-	void SetIfVariable(bool isVariable);
-	/**
-	 * \brief Get the name of the item.
-	 * \return text string containing the name of the item
-	 */
-	const wxString& GetName();
-	/**
-	 * \brief Get the identification number associated with type of the item.
-	 */
-	int GetType();
-	/**
-	 * \brief Tells if the item is filled from data object or variable.
-	 * \return TRUE if it is filled, FALSE if not
-	 */
-	bool IsVariable();
-	/**
-	 * \brief Refresh item's value if it is filled from data source object.
-	 */
-	virtual void RefreshVariable() {;}
-	/**
-	 * \brief Remove content of the item.
-	 */
-	virtual void Clear() {;}
-	/**
-	 * \brief Create XML node of the item - returns NULL for this class.
-	 * \return ever returns NULL
-	 */
-	virtual wxXmlNode* CreateXmlNode() {return NULL;}
-	/**
-	 * \brief Retrieve item from the specified node - returns FALSE for this class.
-	 */
-	virtual bool RetrieveFromXmlNode(const wxXmlNode* node) {return false;}
-	/**
-	 * \brief Draw item to the specified device context - does nothing for this class.
-	 * \param dc device context to draw
-	 * \param toScreen tells if the drawing will be on the screnn or on the printer.
-	 * \param pageStyle used page style.
-	 */
-	virtual void DrawToDC(wxDC* dc, bool toScreen, const wxReportPageStyle& pageStyle) {;}
-	
-	FRIENDS
-	friend class wxReportDocument;
+    /**
+     * \brief Default constructor.
+     */
+    wxReportPageItem();
+    /**
+     * \brief Constructor.
+     * \param name name of the item
+     * \param isVariable tells whether the item will be connected with data source object
+     */
+    wxReportPageItem(const wxString& name, bool isVariable = false);
+    /**
+     * \brief Virtual destructor.
+     */
+    virtual ~wxReportPageItem() {;}
+    /**
+     * \brief Set name of the item.
+     * \param name name of the item
+     */
+    void SetName(const wxString& name);
+    /**
+     * \brief Sets if the item will be filled from data.
+     * \param isVariable TRUE to enable, FALSE to disable
+     */
+    void SetIfVariable(bool isVariable);
+    /**
+     * \brief Get the name of the item.
+     * \return text string containing the name of the item
+     */
+    const wxString& GetName();
+    /**
+     * \brief Get the identification number associated with type of the item.
+     */
+    int GetType();
+    /**
+     * \brief Tells if the item is filled from data object or variable.
+     * \return TRUE if it is filled, FALSE if not
+     */
+    bool IsVariable();
+    /**
+     * \brief Refresh item's value if it is filled from data source object.
+     */
+    virtual void RefreshVariable() {;}
+    /**
+     * \brief Remove content of the item.
+     */
+    virtual void Clear() {;}
+    /**
+     * \brief Create XML node of the item - returns NULL for this class.
+     * \return ever returns NULL
+     */
+    virtual wxXmlNode* CreateXmlNode() {return NULL;}
+    /**
+     * \brief Retrieve item from the specified node - returns FALSE for this class.
+     */
+    virtual bool RetrieveFromXmlNode(const wxXmlNode* node) {return false;}
+    /**
+     * \brief Draw item to the specified device context - does nothing for this class.
+     * \param dc device context to draw
+     * \param toScreen tells if the drawing will be on the screnn or on the printer.
+     * \param pageStyle used page style.
+     */
+    virtual void DrawToDC(wxDC* dc, bool toScreen, const wxReportPageStyle& pageStyle) {;}
+    
+    FRIENDS
+    friend class wxReportDocument;
 };
 
 WX_DECLARE_USER_EXPORTED_OBJARRAY(wxReportPageItem*, ItemsArray, WXDLLIMPEXP_RP);
@@ -185,38 +185,38 @@ WX_DECLARE_USER_EXPORTED_OBJARRAY(wxReportPageItem*, ItemsArray, WXDLLIMPEXP_RP)
 class WXDLLIMPEXP_RP wxReportPositionedItem : public wxReportPageItem
 {
 protected:
-	wxRealPoint m_position;
-	
+    wxRealPoint m_position;
+    
 public:
-	/*!
-	 * \brief Default construstor.
-	 */
-	wxReportPositionedItem();
-	/*!
-	 * \brief Constructor.
-	 * \param name Item's name
-	 * \param x horizontal position of the item
-	 * \param y vertical position of the item
-	 * \param isVariable tells whether the item will be connected with data source object
-	 */
-	wxReportPositionedItem(const wxString& name, double x, double y, bool isVariable);
-	virtual ~wxReportPositionedItem() {;}
-	/**
-	 * \brief Sets the position of the item.
-	 * \param posX horizontal position of the item
-	 * \param posY vertical position of the item
-	 */
-	void SetPosition(double posX, double posY);
-	/*!
-	 * \brief Sets the position of the item.
-	 * \param position item's position
-	 */
-	void SetPosition(const wxRealPoint& position);
-	/*!
-	 * \brief Get the position of the item
-	 * \return current item's position
-	 */
-	const wxRealPoint& GetPosition();
+    /*!
+     * \brief Default construstor.
+     */
+    wxReportPositionedItem();
+    /*!
+     * \brief Constructor.
+     * \param name Item's name
+     * \param x horizontal position of the item
+     * \param y vertical position of the item
+     * \param isVariable tells whether the item will be connected with data source object
+     */
+    wxReportPositionedItem(const wxString& name, double x, double y, bool isVariable);
+    virtual ~wxReportPositionedItem() {;}
+    /**
+     * \brief Sets the position of the item.
+     * \param posX horizontal position of the item
+     * \param posY vertical position of the item
+     */
+    void SetPosition(double posX, double posY);
+    /*!
+     * \brief Sets the position of the item.
+     * \param position item's position
+     */
+    void SetPosition(const wxRealPoint& position);
+    /*!
+     * \brief Get the position of the item
+     * \return current item's position
+     */
+    const wxRealPoint& GetPosition();
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -229,96 +229,96 @@ public:
 class WXDLLIMPEXP_RP wxReportImageItem : public wxReportPositionedItem
 {
 protected:
-	wxReportImageStyle m_style;
-	int m_iWidth;
-	int m_iHeight;
-	int m_iPPI;
-		
+    wxReportImageStyle m_style;
+    int m_iWidth;
+    int m_iHeight;
+    int m_iPPI;
+        
 public:
-	/**
-	 * \brief Default constructor.
-	 */
-	wxReportImageItem();
-	/**
-	 * \brief Constructor.
-	 * \param name name of the image item
-	 * \param path path to the image file
-	 * \param x horizontal position of the image
-	 * \param y vertical position of the image
-	 * \param PPI pixels per inch value of the image
-	 * \param isVariable tells whether the path of the image file will be loaded from the wxString source object
-	 */
-	wxReportImageItem(const wxString& name, const wxString& path, double x, double y, int PPI = 72, bool isVariable = false);
-	/**
-	 * \brief Default destructor.
-	 */
-	virtual ~wxReportImageItem() {;}
-	/**
-	 * \brief Sets the path to the image file.
-	 * \param path path to the image file
-	 */
-	void SetPath(const wxString& path);
-	/**
-	 * \brief Sets the PPI value of the image
-	 * \param ppi pixels per inch of the image
-	 */
-	void SetPPI(int ppi);
-	/**
-	 * \brief Sest the style of the image.
-	 * \param style image style to set
-	 */
-	void SetStyle(const wxReportImageStyle& style);
-	/**
-	 * \brief Returns the path to the image file.
-	 * \return path to the image file
-	 */
-	const wxString& GetPath();
-	/**
-	 * \brief Returns the PPI value of the image.
-	 * \return pixels per inch value of the image
-	 */
-	int GetPPI();
-	/**
-	 * \brief Returns reference to the style associated with the image.
-	 * \return reference to the style object of the item
-	 */
-	wxReportImageStyle& GetStyle();
-	/**
-	 * \brief Returns the width of the image in pixels.
-	 * \return width of the image
-	 */
-	int GetWidth();
-	/**
-	 * \brief Returns the height of the image in pixels.
-	 * \return height of the image
-	 */
-	int GetHeight();
-	/**
-	 * \brief Refresh the path to the image file, if it's filled from the data object.
-	 */
-	void RefreshVariable();
-	/**
-	 * \brief Clears the image - path to image file sets to empty string, the height and the width sets both to 0 and the pointer to the source data object sets to NULL
-	 */
-	void Clear();
-	/**
-	 * \brief Creates the XML node with image parameters.
-	 * \return XML node for the current image
-	 */
-	wxXmlNode* CreateXmlNode();
-	/**
-	 * \brief Retrieves image item from the specified XML node
-	 * \return TRUE on success, FALSE if fails
-	 */
-	bool RetrieveFromXmlNode(const wxXmlNode* node);
-	/**
-	 * \brief Draw image to the specified device context.
-	 * \param dc device context for drawing
-	 * \param toScreen tells if the image will be drawn to the screen or to the printer 
-	 */
-	void DrawToDC(wxDC *dc, bool toScreen, const wxReportPageStyle& pageStyle);
-	
-	FRIENDS
+    /**
+     * \brief Default constructor.
+     */
+    wxReportImageItem();
+    /**
+     * \brief Constructor.
+     * \param name name of the image item
+     * \param path path to the image file
+     * \param x horizontal position of the image
+     * \param y vertical position of the image
+     * \param PPI pixels per inch value of the image
+     * \param isVariable tells whether the path of the image file will be loaded from the wxString source object
+     */
+    wxReportImageItem(const wxString& name, const wxString& path, double x, double y, int PPI = 72, bool isVariable = false);
+    /**
+     * \brief Default destructor.
+     */
+    virtual ~wxReportImageItem() {;}
+    /**
+     * \brief Sets the path to the image file.
+     * \param path path to the image file
+     */
+    void SetPath(const wxString& path);
+    /**
+     * \brief Sets the PPI value of the image
+     * \param ppi pixels per inch of the image
+     */
+    void SetPPI(int ppi);
+    /**
+     * \brief Sest the style of the image.
+     * \param style image style to set
+     */
+    void SetStyle(const wxReportImageStyle& style);
+    /**
+     * \brief Returns the path to the image file.
+     * \return path to the image file
+     */
+    const wxString& GetPath();
+    /**
+     * \brief Returns the PPI value of the image.
+     * \return pixels per inch value of the image
+     */
+    int GetPPI();
+    /**
+     * \brief Returns reference to the style associated with the image.
+     * \return reference to the style object of the item
+     */
+    wxReportImageStyle& GetStyle();
+    /**
+     * \brief Returns the width of the image in pixels.
+     * \return width of the image
+     */
+    int GetWidth();
+    /**
+     * \brief Returns the height of the image in pixels.
+     * \return height of the image
+     */
+    int GetHeight();
+    /**
+     * \brief Refresh the path to the image file, if it's filled from the data object.
+     */
+    void RefreshVariable();
+    /**
+     * \brief Clears the image - path to image file sets to empty string, the height and the width sets both to 0 and the pointer to the source data object sets to NULL
+     */
+    void Clear();
+    /**
+     * \brief Creates the XML node with image parameters.
+     * \return XML node for the current image
+     */
+    wxXmlNode* CreateXmlNode();
+    /**
+     * \brief Retrieves image item from the specified XML node
+     * \return TRUE on success, FALSE if fails
+     */
+    bool RetrieveFromXmlNode(const wxXmlNode* node);
+    /**
+     * \brief Draw image to the specified device context.
+     * \param dc device context for drawing
+     * \param toScreen tells if the image will be drawn to the screen or to the printer 
+     */
+    void DrawToDC(wxDC *dc, bool toScreen, const wxReportPageStyle& pageStyle);
+    
+    FRIENDS
 };
 
 WX_DECLARE_EXPORTED_STRING_HASH_MAP(wxReportPageItem*, ItemsMap);
